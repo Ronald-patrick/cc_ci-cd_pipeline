2eac4aff3f5ac5469561ae11aab19f878af5073d
Ronald Patrick <ronaldpatrick@Ronalds-MacBook-Air.local>
2022-04-11 12:05:13 +0530

    v2

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
 const app = express()
+const path = require('path');
+
 
 app.get("/home" , (req,res)=>{
     res.json({"message":'Hello World',"version" :"v1"})
 })
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
 
 app.listen(PORT,()=>{
 	console.log("Server Started at port :",PORT);
