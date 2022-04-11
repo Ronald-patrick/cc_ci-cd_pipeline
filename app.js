8a7afde7c8248ceb757c80ee85373a1b375df180
Ronald Patrick <ronaldpatrick@Ronalds-MacBook-Air.local>
2022-04-11 11:33:16 +0530

    added port

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
-
+const PORT = process.env.PORT || 5000
 const app = express()
 
 app.get("/home" , (req,res)=>{
     res.json({"message":'Hello World',"version" :"v1"})
 })
 
-app.listen(5000,()=>{
-	console.log("Server Started at port :",5000);
+app.listen(PORT,()=>{
+	console.log("Server Started at port :",PORT);
 })
